84e491fbfc3fbae908d444b4c1ca41b77df3b8c9
aldiaza <alr.diaza@gmail.com>
2022-11-23 17:50:07 -0300

    controllers/mailing update

--- controllers/mailing.js
+++ controllers/mailing.js
@@ -5,8 +5,7 @@ const Mailer = require('../models/mail.js');
 module.exports = {
     get: async (req = request, res = response) => {
         res.status(code.OK)
-            .json({ msg: 'No tengo nada que mostrar - get' })
-            .end();
+            .json({ msg: 'No tengo nada que mostrar - get' });
     },
     email: async (req = request, res = response) => {
         try {
@@ -14,24 +13,21 @@ module.exports = {
             template = `Mi nombre es ${name}, me comunico desde ${email} \n ${message}`;
             const mail = new Mailer('gmail', template);
             mail.sendMail();
-            res.status(code.OK).json({ msg: 'Email enviado con exito' }).end();
+            res.status(code.OK).json({ msg: 'Email enviado con exito' });
         } catch (error) {
             console.log(error);
             res.status(code.INTERNAL_SERVER_ERROR)
                 .json({
                     error: 'Hubo un error al enviar el email',
-                })
-                .end();
+                });
         }
     },
     put: async (req = request, res = response) => {
         res.status(code.OK)
-            .json({ msg: 'No tengo nada que mostrar - put' })
-            .end();
+            .json({ msg: 'No tengo nada que mostrar - put' });
     },
     delete: async (req = request, res = response) => {
         res.status(code.OK)
-            .json({ msg: 'No tengo nada que mostrar - delete' })
-            .end();
+            .json({ msg: 'No tengo nada que mostrar - delete' });
     },
 };
