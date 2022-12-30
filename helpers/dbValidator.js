d1357412749f56345af3c00bc6cc19fd1cb87cf5
aldiaza <alr.diaza@gmail.com>
2022-12-30 11:27:26 -0300

    dbvalidator postulant bug fixed II

--- helpers/dbValidator.js
+++ helpers/dbValidator.js
@@ -2,7 +2,7 @@ const query = require('../services/query')
 
 module.exports = {
     emailExist: async (email = '') => {
-        const emailExist = await query.getOneDBvalidator("email", "postulant", `email = '${email}'`)
+        const emailExist = await query.getOneDBvalidator("postulant", `email = '${email}'`)
         if(emailExist.includes(email)){
             throw new Error(
                 `El email ${email}, ya se encuentra resgitrado.`
