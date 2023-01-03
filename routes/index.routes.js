ddcb548a1b25d3be3fe96c5b8e1bfabc540650af
aldiaza <alr.diaza@gmail.com>
2023-01-03 12:09:31 -0300

    applicant removed bug fixed

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 
 //import especific routes from routes directory
-const applicantRoutes = require('./applicant');
 const recruiterRoutes = require('./recruiter');
 const jobRoutes = require('./job');
 const postulantRoutes = require('./postulant');
@@ -11,7 +10,6 @@ const jobTypeRoutes = require('./jobType');
 const router = Router();
 
 //router.use('/', route.js)
-router.use('/applicant', applicantRoutes);
 router.use('/recruiter', recruiterRoutes);
 router.use('/job', jobRoutes);
 router.use('/postulant', postulantRoutes);
