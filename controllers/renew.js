f7cf423fdbbc61e2b005e41eb2fb0e7d0ef98a51
aldiaza <alr.diaza@gmail.com>
2023-01-05 11:30:37 -0300

    update query updated

--- controllers/renew.js
+++ controllers/renew.js
@@ -17,7 +17,7 @@ module.exports = {
     getPostulant: async ({user} = request, res = response) => {
         try {
             const today = new Date(Date.now())
-            const result = await query.update('postulant', `verified = true, verifiedAt = "${today.toISOString()}"`, `id = ${user.id}`)
+            const result = await query.update('postulant', `verified = true, verifiedAt = "${today.toISOString()}"`, `id = '${user.id}'`)
             return res.status(code.OK)
                 .json({result});
         } catch (error) {
