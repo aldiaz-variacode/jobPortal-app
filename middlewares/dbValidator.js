6eacf798642dda15cdad8785d848f9d5b909e3ed
aldiaza <alr.diaza@gmail.com>
2023-01-05 13:25:43 -0300

    postulant login test middleware login updated

--- middlewares/dbValidator.js
+++ middlewares/dbValidator.js
@@ -15,7 +15,6 @@ module.exports = {
     },
     isVerified: async (req = request, res = response, next) => {
         const {email} = req.body
-        console.log(email)
         const queryString = `SELECT * FROM postulant WHERE email = '${email}';`
         const [isVerified] = await query.get(queryString);
         console.log(isVerified)
