11f440ff21d9da112b6707fe7880b82c40e847d4
aldiaza <alr.diaza@gmail.com>
2023-01-09 11:12:11 -0300

    job query sql updated

--- controllers/job.js
+++ controllers/job.js
@@ -33,7 +33,7 @@ module.exports = {
     },
     get: async (req = request, res = response) => {
         try {
-            const queryString = `SELECT job.id, job.description, job.position, job.location, r.name || ' ' || r.lastname as recruiter, jt.type FROM job INNER JOIN recruiter as r ON job.recruiterid = r.id INNER JOIN jobtype as jt ON job.jobtypeid = jt.id;`
+            const queryString = `SELECT job.id, job.description, job.position, job.location, r.name || ' ' || r.lastname as recruiter, jt.type, jat.type FROM job INNER JOIN recruiter as r ON job.recruiterid = r.id INNER JOIN jobtype as jt ON job.jobtypeid = jt.id INNER JOIN accesstype as jat ON job.accesstypeid = jat.id;`
             const result = await query.get(queryString);
             return res.status(code.OK)
                 .json({ msg: 'Accion exitosa', registros: result });
