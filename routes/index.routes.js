2df604e75ce88fc3e50a0d67107220483d117bf1
aldiaza <alr.diaza@gmail.com>
2023-01-09 11:04:38 -0300

    job access type route added

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -8,6 +8,7 @@ const postulationRoutes = require('./postulation');
 const roleRoutes = require('./role');
 const jobTypeRoutes = require('./jobType');
 const renewRoutes = require('./renew');
+const accessTypeRoutes = require('./accessType');
 const router = Router();
 
 //router.use('/', route.js)
@@ -18,6 +19,7 @@ router.use('/postulation', postulationRoutes);
 router.use('/role', roleRoutes);
 router.use('/jobType', jobTypeRoutes);
 router.use('/renew', renewRoutes);
+router.use('/jobAccessType', accessTypeRoutes);
 
 
 module.exports = router;
