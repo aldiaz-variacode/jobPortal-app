4e19e55e109f2a29796ef3c0321fb821d835a17b
aldiaza <alr.diaza@gmail.com>
2023-01-09 16:01:12 -0300

    getByRecruiterId without log

--- controllers/job.js
+++ controllers/job.js
@@ -58,8 +58,7 @@ module.exports = {
     },
     getByRecruiterId: async ({user} = request, res = response) => {
         try {
-            console.log(user)
-            const queryString = `SELECT * FROM job WHERE recruiterid = '${user.recruiterId}';`
+            const queryString = `SELECT * FROM job WHERE recruiterid = '${user.id}';`
             const result = await query.get(queryString);
             return res.status(code.OK)
                 .json({ msg: 'Accion exitosa', registros: result });
