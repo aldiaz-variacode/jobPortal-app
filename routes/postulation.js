319683a56b972fb4f2886d4014a308d93cb42961
aldiaza <alr.diaza@gmail.com>
2023-01-11 10:40:10 -0300

    /postulation/:postulantid route

--- routes/postulation.js
+++ routes/postulation.js
@@ -22,7 +22,7 @@ router
     ],postulationController.getOne)
 
 router
-    .route('postulant/:postulantid')
+    .route('/postulant/:postulantid')
     .get([
         param('postulantid', 'El id del postulante es requerido').notEmpty(),
     ], postulationController.getByPotulantId)
